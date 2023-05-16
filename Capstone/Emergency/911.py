4487a05e793340efaaeb71ec569701bf8ebc7da8
Kofi Boateng <kdboat2@gmail.com>
2023-05-16 17:29:32 -0500

    Added 911 dataset and updated readme.

--- Capstone/Emergency/911.py
+++ Capstone/Emergency/911.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # 911 Calls Capstone Project
 
 # For this capstone project we will be analyzing some 911 call data from [Kaggle](https://www.kaggle.com/mchirico/montcoalert). The data contains the following fields:
@@ -15,7 +12,6 @@
 # * addr: String variable, Address
 # * e: String variable, Dummy variable (always 1)
 # 
-# Just go along with this notebook and try to complete the instructions or answer the questions in bold using your Python and Data Science skills!
 
 # ## Data and Setup
 
@@ -23,190 +19,97 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# ** Read in the csv file as a dataframe called df **
+
 df = pd.read_csv("911.csv")
 
-
-# ** Check the info() of the df **
 df.info()
 
-
-# ** Check the head of df **
 df.head()
 
 
 # ## Basic Questions
 
 # ** What are the top 5 zipcodes for 911 calls? **
-df["zip"].value_counts().head(5)
+# df["zip"].value_counts().head(5)
 
 # ** What are the top 5 townships (twp) for 911 calls? **
-df["twp"].value_counts().head(5)
+# df["twp"].value_counts().head(5)
 
 # ** Take a look at the 'title' column, how many unique title codes are there? **
-
-df["title"].nunique()
+# df["title"].nunique()
 
 # ## Creating new features
-
-# ** In the titles column there are "Reasons/Departments" specified before the title code. These are EMS, Fire, and Traffic. Use .apply() with a custom lambda expression to create a new column called "Reason" that contains this string value.** 
-# 
-# **For example, if the title column value is EMS: BACK PAINS/INJURY , the Reason column value would be EMS. **
-
 df["Reason"] = df["title"].apply(lambda title: title.split(":")[0])
 
 
 # ** What is the most common Reason for a 911 call based off of this new column? **
-
-df["Reason"].value_counts().head(3)
+# df["Reason"].value_counts().head(3)
 
 # ** Now use seaborn to create a countplot of 911 calls by Reason. **
-
 sns.countplot(x="Reason", data=df, palette='viridis')
-plt.savefig("Reasons.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
-# ___
-# ** Now let us begin to focus on time information. What is the data type of the objects in the timeStamp column? **
 
-
-type(df["timeStamp"].iloc[0])
-
-
-# ** You should have seen that these timestamps are still strings. Use [pd.to_datetime](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.to_datetime.html) to convert the column from strings to DateTime objects. **
+# plt.savefig("Reasons.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+# type(df["timeStamp"].iloc[0])
 
 df["timeStamp"] = pd.to_datetime(df["timeStamp"])
-
-# ** You can now grab specific attributes from a Datetime object by calling them. For example:**
-# 
-#     time = df['timeStamp'].iloc[0]
-#     time.hour
-# 
-# **You can use Jupyter's tab method to explore the various attributes you can call. Now that the timestamp column are actually DateTime objects, use .apply() to create 3 new columns called Hour, Month, and Day of Week. You will create these columns based off of the timeStamp column, reference the solutions if you get stuck on this step.**
-
 
 df["Hour"]= df["timeStamp"].apply(lambda time: time.hour )
 df["Month"]= df["timeStamp"].apply(lambda time: time.month )
 df["Day of Week"]= df["timeStamp"].apply(lambda time: time.dayofweek)
 
 
-# ** Notice how the Day of Week is an integer 0-6. Use the .map() with this dictionary to map the actual string names to the day of the week: **
-# 
-#     dmap = {0:'Mon',1:'Tue',2:'Wed',3:'Thu',4:'Fri',5:'Sat',6:'Sun'}
-
-dmap = {0:'Mon',1:'Tue',2:'Wed',3:'Thu',4:'Fri',5:'Sat',6:'Sun'}
-
-
-df["Day of Week"] = df["Day of Week"].map(dmap)
-
-
-# ** Now use seaborn to create a countplot of the Day of Week column with the hue based off of the Reason column. **
+df["Day of Week"] = df["Day of Week"].map({0:'Mon',1:'Tue',2:'Wed',3:'Thu',4:'Fri',5:'Sat',6:'Sun'})
 
 
 sns.countplot(x="Day of Week", data =df, hue="Reason", palette='viridis')
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
-plt.savefig("ReasonsPerWeek.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
-
-
-
-# **Now do the same for Month:**
+# plt.savefig("ReasonsPerWeek.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
 
 sns.countplot(x="Month", data =df, hue="Reason", palette='viridis')
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
-plt.savefig("ReasonsPerMonth.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
-
-
-# **Did you notice something strange about the Plot?**
-# 
-# _____
-# 
-# ** You should have noticed it was missing some Months, let's see if we can maybe fill in this information by plotting the information in another way, possibly a simple line plot that fills in the missing months, in order to do this, we'll need to do some work with pandas... **
-
-# ** Now create a gropuby object called byMonth, where you group the DataFrame by the month column and use the count() method for aggregation. Use the head() method on this returned DataFrame. **
+# plt.savefig("ReasonsPerMonth.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
 
 byMonth = df.groupby('Month').count()
-byMonth.head()
-
-# ** Now create a simple plot off of the dataframe indicating the count of calls per month. **
-
-byMonth['twp'].plot()
-
-# ** Now see if you can use seaborn's lmplot() to create a linear fit on the number of calls per month. Keep in mind you may need to reset the index to a column. **
+# byMonth.head()
+# byMonth['twp'].plot()
 
 
 sns.lmplot(x="Month", y="twp", data=byMonth.reset_index())
-plt.savefig("TwpVsMonthLF.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+# plt.savefig("TwpVsMonthLF.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
 
-
-# **Create a new column called 'Date' that contains the date from the timeStamp column. You'll need to use apply along with the .date() method. ** 
-
-# In[60]:
 
 
 df['Date']= df["timeStamp"].apply(lambda t: t.date())
-
-
-# ** Now groupby this Date column with the count() aggregate and create a plot of counts of 911 calls.**
 
 t= df.groupby('Date').count()['twp']
 plt.figure(figsize=(10,5))
 plt.title("Timestamp of emergencies")
 plt.plot(t)
 plt.tight_layout()
-plt.savefig("TimestampsOfEmergencies.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+# plt.savefig("TimestampsOfEmergencies.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+
+def callsByReason(reason: str) -> None:
+    t = df[df["Reason"]== f'{reason}'].groupby('Date').count()['twp']
+    plt.figure(figsize=(10,5))
+    plt.title(f'{reason}')
+    plt.plot(t)
+    plt.tight_layout()
+    # plt.savefig(f'{reason}Calls.png', dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+
+reasons = ["Traffic","Fire","EMS"]
+for r in reasons: callsByReason(r)
 
 
+#  Now let's move on to creating  heatmaps with seaborn and our data. We'll first need to restructure the dataframe so that the columns become the Hours and the Index becomes the Day of the Week. There are lots of ways to do this, but I would recommend trying to combine groupby with an [unstack](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.unstack.html) method.
 
-# ** Now recreate this plot but create 3 separate plots with each plot representing a Reason for the 911 call**
+def week_hour_heatmaps(interval: str) -> None:
+    i = df.groupby(by=["Day of Week", f'{interval}']).count()['Reason'].unstack()
 
-# TRAFFIC
-t = df[df["Reason"]== 'Traffic'].groupby('Date').count()['twp']
-plt.figure(figsize=(10,5))
-plt.title("Traffic")
-plt.plot(t)
-plt.tight_layout()
-plt.savefig("TrafficCalls.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+    plt.figure(figsize=(12,6))
+    sns.heatmap(i, cmap='viridis')
+    plt.savefig(f'{interval}Heatmap.png', dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+    sns.clustermap(i, cmap='viridis')
 
-# FIRE
-t= df[df["Reason"]== 'Fire'].groupby('Date').count()['twp']
-plt.figure(figsize=(10,5))
-plt.title("Fire")
-plt.plot(t)
-plt.tight_layout()
-plt.savefig("FireCalls.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
+intervals = ["Hour","Month"]
 
-# EMS
-t = df[df["Reason"]== 'EMS'].groupby('Date').count()['twp']
-plt.figure(figsize=(12,6))
-plt.title("EMS")
-plt.plot(t)
-plt.tight_layout()
-plt.savefig("EMSCalls.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
-
-# ____
-# ** Now let's move on to creating  heatmaps with seaborn and our data. We'll first need to restructure the dataframe so that the columns become the Hours and the Index becomes the Day of the Week. There are lots of ways to do this, but I would recommend trying to combine groupby with an [unstack](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.unstack.html) method. Reference the solutions if you get stuck on this!**
-
-dayHour = df.groupby(by=["Day of Week", "Hour"]).count()['Reason'].unstack()
-dayHour.head()
-
-plt.figure(figsize=(12,6))
-sns.heatmap(dayHour, cmap='viridis')
-plt.savefig("dayHourHeatmap.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
-
-# ** Now create a clustermap using this DataFrame. **
-
-sns.clustermap(dayHour, cmap='viridis')
-
-# ** Now repeat these same plots and operations, for a DataFrame that shows the Month as the column. **
-
-
-dayMonth = df.groupby(by=["Day of Week", "Month"]).count()['Reason'].unstack()
-dayMonth.head()
-
-plt.figure(figsize=(12,6))
-sns.heatmap(dayMonth, cmap='viridis')
-plt.savefig("dayMonthHeatmap.png", dpi=300, facecolor="white", bbox_inches="tight" ,transparent=False)
-
-
-
-
-# **Continue exploring the Data however you see fit!**
-# # Great Job!
+for i in intervals: week_hour_heatmaps(i)
--- Capstone/Finance/stocks.py
+++ Capstone/Finance/stocks.py
@@ -1,19 +1,3 @@
-# # Finance Data Project - Solutions
-# 
-# In this data project we will focus on exploratory data analysis of stock prices. Keep in mind, this project is just meant to practice your visualization and pandas skills, it is not meant to be a robust financial analysis or be taken as financial advice.
-
-# We'll focus on bank stocks and see how they progressed throughout the [financial crisis](https://en.wikipedia.org/wiki/Financial_crisis_of_2007%E2%80%9308) all the way to early 2016.
-
-# ## Get the Data
-# 
-# In this section we will use pandas to directly read data from Google finance!
-# 
-# First we need to start with the proper imports, which we've already laid out for you here.
-# 
-# *Note: [You'll need to install pandas-datareader for this to work!](https://github.com/pydata/pandas-datareader) Pandas datareader allows you to [read stock information directly from the internet](http://pandas.pydata.org/pandas-docs/stable/remote_data.html) Use these links for install guidance (**pip install pandas-datareader**), or just follow along with the video lecture.*
-# 
-
-
 from pandas_datareader import data
 import pandas as pd
 import datetime
